cd1e4d74038f5e5396fd2a904bb19448971cb66c
Dmitry Yarmiychuk <yarmiychuk@gmail.com>
2022-07-13 15:46:23 +0300

    Second From Three app added

---
+++ 10_Second_from_three/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число
+и на выходе показывает вторую цифру этого числа.
+*/
+
+int GetSecondDigit(int number)
+{
+    return number / 10 % 10;
+}
+
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100 || number >= 1000)
+    Console.WriteLine("Вы ввели неправильное число");
+else
+    Console.WriteLine($"Вторая цифра: {GetSecondDigit(number)}");
