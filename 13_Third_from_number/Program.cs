24d558786a73a5930789e4714dd2260b40e59fdf
Dmitry Yarmiychuk <yarmiychuk@gmail.com>
2022-07-13 16:00:38 +0300

    Third From Number app added

---
+++ 13_Third_from_number/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного числа
+или сообщает, что третьей цифры нет.
+*/
+
+int GetThirdDigit(int number)
+{
+    while (number >= 1000)
+    {
+        number /= 10;
+    }
+
+    return number % 10;
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100)
+    Console.WriteLine($"У числа {number} третьей цифры нет!");
+else
+    Console.WriteLine($"Третья цифра: {GetThirdDigit(number)}");
